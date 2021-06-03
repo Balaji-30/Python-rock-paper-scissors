f35d4a63f79b4cb08ebe3c9545a30df7c06c546d
Balaji Senthilkumar <74751210+Balaji-30@users.noreply.github.com>
2021-06-04 00:26:30 +0530

    Update rps.py

--- rps.py
+++ rps.py
@@ -3,22 +3,16 @@ print("Enter your name:",end="")
 name=input()
 print("\nHello,",name)
 a=[]
-n=False
 ch=input()
 ch=ch.replace("'","").replace("[","").replace("]","").replace(" ","").replace("\"","")
 print("Okay, let's start")
 if ch=="":
-    n=True
-    a=["rock","paper","scissors"]
-    lose_conditions = {"rock": "paper", "paper": "scissors", "scissors": "rock"}
-else:
-    lose_conditions={}
-    
+    ch="rock,paper,scissor"    
 file=open("rating.txt","r")
 j=[]
 for x in file:
-    
     j.append(x.split(" "))
+print(j)
 y=len(j)
 g=0
 for i in range(y):
@@ -32,50 +26,30 @@ if f==0:
     g=0
     
 while 1:
-    new = input()
-    if n:
-        choice = random.choice(a)
+        new = input()
+        a=ch.split(",")
+        b=ch.split(",")
         if new == '!exit':
-            print('Bye!')
-            break
+             print('Bye!')
+             break
         elif new == "!rating":
             print("Your rating:",end="")
-            print(g)           
-        elif new not in a:
+            print(g)
+            continue
+        elif new not in b:
             print("Invalid input")
-        elif lose_conditions[new] == choice :
-            print("Sorry, but the computer chose " + choice)
-        elif new == choice :
+            continue
+        choice = random.choice(a)
+        x=a.index(new)
+        a=a[x+1:]
+        a.extend(b[:x])
+        l=len(a)//2
+        a=a[:l]          
+        if  new == choice :
             print('There is a draw (' + choice + ')')
-            g+=50
+            g+=50    
+        elif choice in a:
+            print("Sorry, but the computer chose " + choice)
         else:
             print('Well done. The computer chose ' + choice + ' and failed')
             g+=100
-    else:
-            a=ch.split(",")
-            b=ch.split(",")
-            if new == '!exit':
-                print('Bye!')
-                break
-            elif new == "!rating":
-                print("Your rating:",end="")
-                print(g)
-                continue
-            elif new not in b:
-                print("Invalid input")
-                continue
-            choice = random.choice(a)
-            x=a.index(new)
-            a=a[x+1:]
-            a.extend(b[:x])
-            l=len(a)//2
-            a=a[:l]          
-            if  new == choice :
-                print('There is a draw (' + choice + ')')
-                g+=50    
-            elif choice in a:
-                print("Sorry, but the computer chose " + choice)
-            else:
-                print('Well done. The computer chose ' + choice + ' and failed')
-                g+=100
-
